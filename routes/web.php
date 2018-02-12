3c215280adbf18960183abebba03054678101810
Jason Piros <jahsan87@gmail.com>
2018-02-11 18:32:52 -0800

    created Task model and migration script

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,16 @@
 |
 */
 
+use App\Task;
+
 Route::get('/tasks', function () {
-    $tasks = DB::table('tasks')->get();
+    $tasks = Task::all();
 
     return view('welcome', compact('tasks'));
 });
 
 Route::get('/tasks/{task}', function ($id) {
-    $task = DB::table('tasks')->find($id);
+    $task = Task::find($id);
 
     return view('tasks.show', compact('task'));
 });
